37ab0bcf13cdc4a03ee6928ffc5cd1c572054682
João <joao.salgueiro@marzee.co>
2024-11-12 17:32:22 +0000

    update socials

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -10,6 +10,7 @@ const socials: Social[] = [
   { name: 'X', url: 'https://x.com/joaodotcodes', color: 'text-[#FF6B6B]' },
   { name: 'github', url: 'https://github.com/nocategory', color: 'text-[#2ECDC2]' },
   { name: 'linkedin', url: 'https://www.linkedin.com/in/jfilsalgueiro/', color: 'text-[#21d286]' },
+  { name: 'polywork', url: 'https://www.polywork.com/joaodotcodes', color: 'text-[#8e99ed]'},
   { name: 'bluesky', url: 'https://bsky.app/profile/joao.codes', color: 'text-[#2153ff]'}
 ];
 
