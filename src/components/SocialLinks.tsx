52a763f7c86d49599f3380c66a928fc1525f22aa
João <joao.salgueiro@marzee.co>
2024-11-12 17:33:03 +0000

    fix socials mistake. oopsie

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 type Social = {
   name: string;
   url: string;
@@ -8,10 +6,26 @@ type Social = {
 
 const socials: Social[] = [
   { name: 'X', url: 'https://x.com/joaodotcodes', color: 'text-[#FF6B6B]' },
-  { name: 'github', url: 'https://github.com/nocategory', color: 'text-[#2ECDC2]' },
-  { name: 'linkedin', url: 'https://www.linkedin.com/in/jfilsalgueiro/', color: 'text-[#21d286]' },
-  { name: 'polywork', url: 'https://www.polywork.com/joaodotcodes', color: 'text-[#8e99ed]'},
-  { name: 'bluesky', url: 'https://bsky.app/profile/joao.codes', color: 'text-[#2153ff]'}
+  {
+    name: 'github',
+    url: 'https://github.com/nocategory',
+    color: 'text-[#2ECDC2]',
+  },
+  {
+    name: 'linkedin',
+    url: 'https://www.linkedin.com/in/jfilsalgueiro/',
+    color: 'text-[#21d286]',
+  },
+  {
+    name: 'polywork',
+    url: 'https://www.polywork.com/js',
+    color: 'text-[#8e99ed]',
+  },
+  {
+    name: 'bluesky',
+    url: 'https://bsky.app/profile/joao.codes',
+    color: 'text-[#2153ff]',
+  },
 ];
 
 export default function SocialLinks() {
